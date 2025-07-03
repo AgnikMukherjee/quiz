c278118ba63dee446701d77376482030de440679
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-07-03 16:51:17 +0530

    client url cors update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ await connectDB()
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', `${CLIENT_URL}`],
+  origin: ['http://localhost:5173', process.env.CLIENT_URL],
   credentials: true
 }));
 
