a53b1e3afada0b6d3f731fef0912293d91226f1b
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-07-05 00:13:15 +0530

    styling update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,30 @@
-const Footer = () => (
-  <footer className="bg-gray-100 text-center py-3 text-gray-600 text-sm">
-    &copy; {new Date().getFullYear()} Quiz App. All rights reserved.
-  </footer>
-);
+import facebook_icon from '../assets/facebook_icon.svg'
+import twitter_icon from '../assets/twitter_icon.svg'
+import instagram_icon from '../assets/instagram_icon.svg'
+import bulb from '../assets/bulb.png'
+const Footer = () => {
+  return (
+    <div className='w-full min-h-12 border-t border-gray-400'>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left px-4'>
+      {/* left */}
+      <div className='flex items-center gap-2'>
+        <img className='hidden md:block w-7 border rounded-full border-gray-400 '
+          src={bulb} alt="" />
+        <div className='hidden md:block h-9 w-px bg-gray-500/60'></div>
+        <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
+          Copyright 2025 © Agnik Mukherjee. | All Rights Reserved.
+        </p>
+      </div>
+      {/* right */}
+      <div className='flex items-center gap-3 max-md:mt-4'>
+        <a href="#"><img src={facebook_icon} alt="" className='w-6'/> </a>
+        <a href="#"><img src={twitter_icon} alt="" className='w-6'/> </a>
+        <a href="#"><img src={instagram_icon} alt="" className='w-6'/> </a>
+      </div>
+    </footer>
+    </div>
 
-export default Footer;
+  )
+}
+
+export default Footer
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
+import bulb from '../assets/bulb.png'
 const Navbar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
@@ -11,14 +11,17 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-600 min-h-[3vw] text-white px-6 py-4  flex justify-between items-center shadow-md">
+    <nav className="bg-gradient-to-b from-blue-500 to-blue-300 min-h-12 text-white px-4 flex justify-between items-center shadow-md">
+      <div className='flex items-center gap-2'>
+      <img src={bulb} alt="" className='w-6'/>
       <h1 className="text-xl font-bold">Quiz</h1>
+      </div>
       
       <div className="flex items-center gap-4 ">
         <span>{user?.role} | {user?.username}</span>
         
         <button
-          className="mt-4 text-sm border border-red-500 rounded px-4 py-2 text-red-500 bg-white hover:bg-red-500 hover:text-white"
+          className="text-sm border border-red-500 rounded px-2 py-1 text-red-500 bg-white hover:bg-red-500 hover:text-white"
           onClick={handleLogout}
         >
           Logout
